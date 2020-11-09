41a80d4d2e72fdc544c9e979b3a9311c5cd758fc
prince <princeibewiro@indecex.com.br>
2020-11-08 23:35:36 -0300

    Message

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const RegistrationQueue = require("./lib/RegistrationQueue");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/user", async (req, res) => {
